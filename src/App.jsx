31a3103c67cf1676706c60f0a1c2100665345478
mhenriquecode <matheucar186@gmail.com>
2025-08-28 13:47:27 -0300

    feat: Implemanting task list and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudos",
+      description: "Estudar programação",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Dois Estudos",
+      description: "Estudar sociais",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Três Estudos",
+      description: "Estudar geografia",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id == taskId) {
+        // ...task = tudo que já estava na tarefa
+        //!task.isCompleted = colocando o valor do insCompleted como True
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
@@ -9,7 +43,7 @@ function App() {
           Gerenciador de Tarefas
         </h1>
         <AddTask />
-        <Tasks />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,26 @@
-function Tasks() {
-  return <h1>Tasks</h1>;
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+
+function Tasks(props) {
+  console.log(props);
+  return (
+    <ul className="space-y-4 p-6 bg-slate-500 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-white text-left w-full p-2 rounded-md ${
+              task.isCompleted && "line-through"
+            }`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;
