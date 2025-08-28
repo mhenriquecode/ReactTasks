1c34e8b4ce3c94d49001cd5d555ac8f1a9c5f517
mhenriquecode <matheucar186@gmail.com>
2025-08-28 14:14:17 -0300

    feat: Implementing add tasks

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,21 @@
 function AddTask() {
-  return <h1>Add Tasks</h1>;
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <input
+        type="text"
+        placeholder="Digite o titulo da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      />
+      <input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      />
+      <button className="bg-slate-600 text-white px-4 py-2 rounded-md font-medium">
+        Adicionar
+      </button>
+    </div>
+  );
 }
 
 export default AddTask;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,7 +2,7 @@ import { ChevronRightIcon, TrashIcon } from "lucide-react";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   return (
-    <ul className="space-y-4 p-6 bg-slate-500 rounded-md shadow">
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
           <button
